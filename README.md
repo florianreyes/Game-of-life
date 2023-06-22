5153aad82d4c991a677754c9e0e9da461d60f89e
Florian <florianreyes541@gmail.com>
2023-06-22 16:23:14 -0300

    game.cpp

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Game-of-life
+Game of Life 🎮
+Welcome to the Game of Life repository! 🌟 This project is an implementation of Conway's Game of Life using C++ and the SFML library.
+
+What is the Game of Life?
+The Game of Life, devised by the mathematician John Horton Conway in 1970, is a cellular automaton. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. The game takes place on an infinite grid of square cells, each of which can be in one of two states: alive or dead. The state of each cell evolves over time according to a set of rules.
+
+Rules of the Game 📖
+The game follows these simple rules:
+
+Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by overpopulation.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+How to Play 🎯
+Setting Up: Initially, you will need to provide an initial configuration of live cells on the grid. This can be done by modifying the source code or by loading predefined configurations.
+Running the Simulation: Once the initial configuration is set, run the program. The cells will evolve following the rules of the game.
+Observe and Enjoy: Sit back and watch the mesmerizing patterns that emerge as the game progresses. You can pause or reset the simulation at any time.
+Installation and Dependencies 🛠️
+To run the Game of Life, you will need to have the following dependencies installed:
+
+C++ Compiler (supporting C++11 or later)
+SFML Library
+Follow these steps to get started:
+
+Clone this repository to your local machine.
+Ensure that the SFML library is correctly installed.
+Build the project using your preferred C++ compiler.
+Run the executable generated.
---
+++ game.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <SFML/Graphics.hpp>
+
+// Dimensiones de la ventana y la cuadrícula
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int CELL_SIZE = 10;
+const int GRID_WIDTH = WIDTH / CELL_SIZE;
+const int GRID_HEIGHT = HEIGHT / CELL_SIZE;
+
+// Función para inicializar la cuadrícula aleatoriamente
+void inicializarCuadricula(bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
+{
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+            cuadricula[i][j] = rand() % 2 == 0;
+        }
+    }
+}
+
+// Función para dibujar la cuadrícula en la ventana
+void dibujarCuadricula(sf::RenderWindow &ventana, bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
+{
+    ventana.clear();
+
+    sf::RectangleShape celda(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+            if (cuadricula[i][j])
+            {
+                celda.setFillColor(sf::Color::White);
+            }
+            else
+            {
+                celda.setFillColor(sf::Color::Black);
+            }
+            celda.setPosition(i * CELL_SIZE, j * CELL_SIZE);
+            ventana.draw(celda);
+        }
+    }
+
+    ventana.display();
+}
+
+// Función para contar los vecinos vivos de una celda
+int contarVecinosVivos(bool cuadricula[GRID_WIDTH][GRID_HEIGHT], int x, int y)
+{
+    int vecinosVivos = 0;
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            if (i == 0 && j == 0)
+                continue;
+            int vecinoX = (x + i + GRID_WIDTH) % GRID_WIDTH;
+            int vecinoY = (y + j + GRID_HEIGHT) % GRID_HEIGHT;
+            if (cuadricula[vecinoX][vecinoY])
+            {
+                vecinosVivos++;
+            }
+        }
+    }
+    return vecinosVivos;
+}
+
+// Función para actualizar la cuadrícula según las reglas del juego de la vida
+void actualizarCuadricula(bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
+{
+    bool nuevaCuadricula[GRID_WIDTH][GRID_HEIGHT];
+
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+            int vecinosVivos = contarVecinosVivos(cuadricula, i, j);
+            if (cuadricula[i][j])
+            {
+                nuevaCuadricula[i][j] = (vecinosVivos == 2 || vecinosVivos == 3);
+            }
+            else
+            {
+                nuevaCuadricula[i][j] = (vecinosVivos == 3);
+            }
+        }
+    }
+
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+            cuadricula[i][j] = nuevaCuadricula[i][j];
+        }
+    }
+}
+
+int main()
+{
+    bool cuadricula[GRID_WIDTH][GRID_HEIGHT];
+    inicializarCuadricula(cuadricula);
+
+    sf::RenderWindow ventana(sf::VideoMode(WIDTH, HEIGHT), "Juego de la Vida");
+
+    while (ventana.isOpen())
+    {
+        sf::Event evento;
+        while (ventana.pollEvent(evento))
+        {
+            if (evento.type == sf::Event::Closed)
+            {
+                ventana.close();
+            }
+        }
+
+        dibujarCuadricula(ventana, cuadricula);
+        actualizarCuadricula(cuadricula);
+
+        usleep(100000); // Pausa de 100 milisegundos
+    }
+
+    return 0;
+}
