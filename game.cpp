715d0d85553a63925f3912e388f6191c6d25ed5a
Florian <florianreyes541@gmail.com>
2023-06-22 22:04:53 -0300

    Color variants

--- game.cpp
+++ game.cpp
@@ -10,44 +10,6 @@ const int CELL_SIZE = 10;
 const int GRID_WIDTH = WIDTH / CELL_SIZE;
 const int GRID_HEIGHT = HEIGHT / CELL_SIZE;
 
-// Función para inicializar la cuadrícula aleatoriamente
-void inicializarCuadricula(bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
-{
-    for (int i = 0; i < GRID_WIDTH; i++)
-    {
-        for (int j = 0; j < GRID_HEIGHT; j++)
-        {
-            cuadricula[i][j] = rand() % 2 == 0;
-        }
-    }
-}
-
-// Función para dibujar la cuadrícula en la ventana
-void dibujarCuadricula(sf::RenderWindow &ventana, bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
-{
-    ventana.clear();
-
-    sf::RectangleShape celda(sf::Vector2f(CELL_SIZE, CELL_SIZE));
-    for (int i = 0; i < GRID_WIDTH; i++)
-    {
-        for (int j = 0; j < GRID_HEIGHT; j++)
-        {
-            if (cuadricula[i][j])
-            {
-                celda.setFillColor(sf::Color::White);
-            }
-            else
-            {
-                celda.setFillColor(sf::Color::Black);
-            }
-            celda.setPosition(i * CELL_SIZE, j * CELL_SIZE);
-            ventana.draw(celda);
-        }
-    }
-
-    ventana.display();
-}
-
 // Función para contar los vecinos vivos de una celda
 int contarVecinosVivos(bool cuadricula[GRID_WIDTH][GRID_HEIGHT], int x, int y)
 {
@@ -67,6 +29,101 @@ int contarVecinosVivos(bool cuadricula[GRID_WIDTH][GRID_HEIGHT], int x, int y)
         }
     }
     return vecinosVivos;
+}
+// Función para inicializar la cuadrícula aleatoriamente
+void inicializarCuadricula(bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
+{
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+            cuadricula[i][j] = rand() % 2;
+        }
+    }
+}
+
+// Función para dibujar la cuadrícula en la ventana
+void dibujarCuadricula(sf::RenderWindow &ventana, bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
+{
+    ventana.clear();
+    sf::RectangleShape celda(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+
+            if (cuadricula[i][j])
+            {
+                celda.setFillColor(sf::Color::White);
+            }
+            else
+            {
+                celda.setFillColor(sf::Color::Black);
+            }
+            celda.setPosition(i * CELL_SIZE, j * CELL_SIZE);
+            ventana.draw(celda);
+        }
+    }
+
+    ventana.display();
+}
+void dibujarCuadriculaRand(sf::RenderWindow &ventana, bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
+{
+    ventana.clear();
+    // randomize color
+    int r = rand() % 255;
+    int g = rand() % 255;
+    int b = rand() % 255;
+
+    sf::RectangleShape celda(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+
+            if (cuadricula[i][j])
+            {
+                celda.setFillColor(sf::Color(r, g, b));
+            }
+            else
+            {
+                celda.setFillColor(sf::Color::Black);
+            }
+            celda.setPosition(i * CELL_SIZE, j * CELL_SIZE);
+            ventana.draw(celda);
+        }
+    }
+
+    ventana.display();
+}
+void dibujarCuadriculaParty(sf::RenderWindow &ventana, bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
+{
+    ventana.clear();
+
+    sf::RectangleShape celda(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+    for (int i = 0; i < GRID_WIDTH; i++)
+    {
+        for (int j = 0; j < GRID_HEIGHT; j++)
+        {
+
+            if (cuadricula[i][j])
+            {
+                // randomize color
+                int r = rand() % 255;
+                int g = rand() % 255;
+                int b = rand() % 255;
+                celda.setFillColor(sf::Color(r, g, b));
+            }
+            else
+            {
+                celda.setFillColor(sf::Color::Black);
+            }
+            celda.setPosition(i * CELL_SIZE, j * CELL_SIZE);
+            ventana.draw(celda);
+        }
+    }
+
+    ventana.display();
 }
 
 // Función para actualizar la cuadrícula según las reglas del juego de la vida
@@ -101,6 +158,15 @@ void actualizarCuadricula(bool cuadricula[GRID_WIDTH][GRID_HEIGHT])
 
 int main()
 {
+    std::cout << "Juego de la vida" << std::endl;
+    std::cout << "Elija el modo" << std::endl
+              << "[1] Clásico" << std::endl
+              << "[2] Color random" << std::endl
+              << "[3] Color Party " << std::endl;
+
+    int modo;
+    std::cin >> modo;
+
     bool cuadricula[GRID_WIDTH][GRID_HEIGHT];
     inicializarCuadricula(cuadricula);
 
@@ -117,7 +183,21 @@ int main()
             }
         }
 
-        dibujarCuadricula(ventana, cuadricula);
+        switch (modo)
+        {
+        case 1:
+            dibujarCuadricula(ventana, cuadricula);
+            break;
+        case 2:
+            dibujarCuadriculaRand(ventana, cuadricula);
+            break;
+        case 3:
+            dibujarCuadriculaParty(ventana, cuadricula);
+            break;
+
+        default:
+            break;
+        }
         actualizarCuadricula(cuadricula);
 
         usleep(100000); // Pausa de 100 milisegundos
